70aa2fa99492590153101ce07763733d8d9615c7
Harding <tujumg@gmail.com>
2019-02-09 12:17:44 +0800

    add something different

--- product.py
+++ product.py
@@ -1,16 +1,26 @@
+# 讀取檔案
 products = []
+with open('products.csv','r',encoding='utf-8')as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue 
+		name,price = line .strip().split(',')  #字串可以用.split來切割
+		products.append([name,price])         #.strip來去掉換行符號(\n)
+print(products)
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱')
 	if name == 'q':
 		break
-	price = input('請輸入價格')
+	price = input('請輸 入價格')
 	price = int(price)
 	products.append([name,price])
 
 print(products)
+#印出所有購買紀錄
 for p in products:
 	print(p[0],'的價格是',p[1]) #小清單裡面第0格是..
-
+#寫入檔案
 with open('products.csv','w',encoding='utf-8 ')as f: #r為讀取,w為寫入模式，就算沒有txt檔,仍然可以寫入,也可寫成.csv的excel檔,utf-8為最廣泛使用的編碼
 	f.write('商品,價格\n')
 	for p in products: 
